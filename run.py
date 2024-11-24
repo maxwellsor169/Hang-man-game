9e7005fc95e7c6dc47b005d5c32d286239a1b45a
Maxwell Sor <atm2chx3@students.codeinstitute.net>
2024-11-24 01:27:42 +0000

    Define the selected word function

--- run.py
+++ run.py
@@ -3,5 +3,10 @@ import string
 
 MAX_INCORRECT_GUESSES = 6
 
+def selected_word():
+    with open("gaming_words.txt", mode="r") as words:
+        word_list = words.readlines()
+    return random.choice(word_list)
+
     
 
