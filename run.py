abfe7baba46ead52c3827a472e997535e83003f0
Maxwell Sor <atm2chx3@students.codeinstitute.net>
2024-11-24 13:38:09 +0000

    Define the player input function

--- run.py
+++ run.py
@@ -1,12 +1,30 @@
 import random
 import string
+from colorama import Fore, init
+
+init()
 
 MAX_INCORRECT_GUESSES = 6
 
+
+
+
 def selected_word():
+    """
+    This function will target the word to be selected by the computer
+    """
     with open("gaming_words.txt", mode="r") as words:
-        word_list = words.readlines()
+       word_list = words.readlines()
     return random.choice(word_list)
 
-    
+def get_player_input(guessed_letters):
+    """
+    Get the guessing player's input
+    """
 
+    while True:
+        print("Your guessed letters should be in small cases")
+        player_input = input("Guess a letter:\n").lower()
+
+        if _validate_input(player_input, guessed_letters):
+            return player_input        
